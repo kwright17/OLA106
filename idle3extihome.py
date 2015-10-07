2b5fbaa525ebb47fc3982e9945bd78e3b4ea3c2f
kwright17 <kw4w@mtmail.mtsu.edu>
2015-10-07 00:23:09 -0500

    Update exitHome function

--- idle3extihome.py
+++ idle3extihome.py
@@ -1,38 +1,37 @@
 
 
+
 ##exitHome
 def exitHome():
     face_north()
     find_vest()
     exit_vest()
-    turn_off
+    turn_off()
 ##turn right
 def turn_right():
     turn_left()
     turn_left()
     turn_left()   
 ##face North 
-def face_north():
-    while not facing_north():
-
-        turn_right()
+    face_north():
+        while not facing_north():
+            turn_left()
 ##find vestibule
 def find_vest():
-    
     while front_is_clear():
         move()
         if  left_is_clear():
             turn_left()
             exit_vest()
+         if not right_is_clear():
+             exit_vest()     
+    turn_right()
         if not right_is_clear():
             exit_vest()
-        if not front_is_clear():
-            
-            turn_right()
 ##Exit Vestibule   
 def exit_vest():
     move()
     if not left_is_clear():
         move()
         
-exitHome()
+    
