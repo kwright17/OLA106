14515ea0a9402446f8993d8ccb1399ab16b39e96
kwright17 <kw4w@mtmail.mtsu.edu>
2015-10-07 16:41:42 -0500

    Added change so Reeborg exits vestibule and "waits"

--- exithome.py
+++ exithome.py
@@ -1,10 +1,10 @@
 
 
+
 ##exitHome
 def exitHome():
     face_north()
-    find_vest()
-    exit_vest()
+    exit()
     turn_off
 ##turn right
 def turn_right():
@@ -13,26 +13,31 @@ def turn_right():
     turn_left()   
 ##face North 
 def face_north():
+    if front_is_clear():
+        exit_vest()
     while not facing_north():
 
-        turn_right()
+        turn_left()
 ##find vestibule
-def find_vest():
-    
-    while front_is_clear():
-        move()
-        if  left_is_clear():
-            turn_left()
-            exit_vest()
-        if not right_is_clear():
-            exit_vest()
+def exit():
+    if left_is_clear():
+        turn_left()
+        exit_vest()
+    while right_is_clear():
         if not front_is_clear():
-            
             turn_right()
+        move()
+    
+    exit_vest()
 ##Exit Vestibule   
 def exit_vest():
+    
     move()
     if not left_is_clear():
         move()
-        
+    
 exitHome()
+
+
+
+
