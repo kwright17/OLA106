0c92c8489cc3ba846e9752f372afda8037b1bbf7
kwright17 <kw4w@mtmail.mtsu.edu>
2015-10-07 22:59:23 -0500

    Add code so Reeborg can Leave any room

--- exithome.py
+++ exithome.py
@@ -1,11 +1,11 @@
 
 
-
 ##exitHome
 def exitHome():
     face_north()
-    exit()
-    turn_off
+    go_outside()
+    exit_vest()
+    turn_off()
 ##turn right
 def turn_right():
     turn_left()
@@ -13,31 +13,35 @@ def turn_right():
     turn_left()   
 ##face North 
 def face_north():
-    if front_is_clear():
-        exit_vest()
-    while not facing_north():
-
-        turn_left()
+    
+        while not facing_north():
+            turn_left()
 ##find vestibule
-def exit():
-    if left_is_clear():
-        turn_left()
-        exit_vest()
-    while right_is_clear():
-        if not front_is_clear():
-            turn_right()
-        move()
+def go_outside():
+    while not left_is_clear():
+        
+        if right_is_clear():
+            if not front_is_clear():
+                turn_right()
+            move()
+        else:
+            move()
     
-    exit_vest()
-##Exit Vestibule   
+        
 def exit_vest():
-    
-    move()
+    if not right_is_clear():
+        move()
+    else:
+        turn_left()
+        move()
     if not left_is_clear():
         move()
+        turn_left()
+        move()
+            
+        
+##Exit Vestibule   
+
+        
     
 exitHome()
-
-
-
-
--- idle3extihome.py
+++ idle3extihome.py
@@ -1,37 +1,34 @@
-
-
-
-##exitHome
-def exitHome():
-    face_north()
-    find_vest()
-    exit_vest()
-    turn_off()
-##turn right
 def turn_right():
     turn_left()
     turn_left()
     turn_left()   
 ##face North 
-    face_north():
+def face_north():
+    
         while not facing_north():
             turn_left()
 ##find vestibule
-def find_vest():
-    while front_is_clear():
-        move()
-        if  left_is_clear():
-            turn_left()
-            exit_vest()
-         if not right_is_clear():
-             exit_vest()     
-    turn_right()
-        if not right_is_clear():
-            exit_vest()
-##Exit Vestibule   
+def go_outside():
+    while not left_is_clear():
+        
+        if right_is_clear():
+            if not front_is_clear():
+                turn_right()
+            move()
+        else:
+            move()
+    
+        
 def exit_vest():
-    move()
+    if not right_is_clear():
+        move()
+    else:
+        turn_left()
+        move()
     if not left_is_clear():
         move()
+        turn_left()
+        move()
+        
         
     
